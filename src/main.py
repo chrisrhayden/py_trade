48aa021836c73a9c63c83952abbfe6796d0069a5
chris <chrisrhayden@gmail.com>
2018-07-21 06:28:28 -0700

    adding logging

--- src/main.py
+++ src/main.py
@@ -2,23 +2,72 @@
 make an exchange class that will run a strategy on a given amount of symbols
 '''
 
+import logging
+
 import ccxt
 
 from PyTrade import PyTrade
+from default_strategy import Strategy
+from csv_utils import write_csv
+
+
+class Coin:
+    def __init__(self, symbol, strategy, filter_func):
+        self.symbol = symbol
+
+        self.StrategyClass = strategy
+        self.strategy = self.StrategyClass.check_strategy
+
+        self.filter_func = filter_func
+
+
+def setup_logger():
+    fmt_str = '[%(asctime)s] - [%(levelname)s] - %(funcName)s - %(message)s'
+    formatter = logging.Formatter(fmt_str)
+
+    # get the logger
+    logger = logging.getLogger('py_trade')
+    logger.setLevel(logging.DEBUG)
+
+    fh = logging.FileHandler('py_trade.log')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.INFO)
+    sh.setFormatter(formatter)
+
+    logger.addHandler(sh)
+    logger.addHandler(fh)
+
+
+def last_candle(candle_list):
+    return candle_list[0]
+
+
+def make_coin_class(symbols, strategy):
+    coins = [Coin(s, strategy, last_candle) for s in symbols]
+
+    return coins
 
 
 def main():
     ''' the start function '''
 
-    exchange = ccxt.poloniex()
+    setup_logger()
+
+    exchange = ccxt.cex({'enableRateLimit': None})
 
     exchange.load_markets()
 
-    bot = PyTrade(
-            exchange,
-            ['BCH/BTC', 'BCH/USD', 'BTC/EUR', 'BTC/GBP', 'BTC/USD',
-             'ETH/BTC', 'ETH/EUR', 'ETH/USD', 'LTC/BTC', 'LTC/EUR',
-             'LTC/USD', 'BCH/EUR'])
+    symbols = ['BTC/USD', 'ETH/USD',
+               'BCH/USD', 'BTG/USD',
+               'DASH/USD', 'XRP/USD',
+               'XLM/USD', 'ZEC/USD']
+
+    coins = make_coin_class(symbols, Strategy())
+
+    bot = PyTrade(exchange, coins, [write_csv])
 
     bot.start_bot()
 
