dc9f5bde70990110974612747dc84d1b27873882
chris <chrisrhayden@gmail.com>
2018-07-21 06:29:12 -0700

    fake trade functions

---
+++ src/default_strategy.py
@@ -0,0 +1,8 @@
+class Strategy:
+    ''' to show the methods and make sure the bot works '''
+
+    def __init__(self):
+        pass
+
+    def check_strategy(self, data):
+        return {'buy': None, 'sell': None}
