9de5b61994730b58a229707fc8c4a64335f0dd6a
chris <chrisrhayden@gmail.com>
2018-07-21 11:58:05 -0700

    renamed symbols

--- src/default_strategy.py
+++ src/default_strategy.py
@@ -1,8 +1,12 @@
 class Strategy:
     ''' to show the methods and make sure the bot works '''
 
-    def __init__(self):
-        pass
+    def __init__(self, pair):
+        self.pairs = pair
+
+    def filter_data(self, unfiltered_data):
+        ''' a conveyance method to keep the strategy func clean '''
+        return unfiltered_data[0]
 
     def check_strategy(self, data):
         return {'buy': None, 'sell': None}
